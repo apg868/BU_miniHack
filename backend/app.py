045645eed4b0b73eb6fe3f105deb715a10d17d60
apg868 <123202954+apg868@users.noreply.github.com>
2023-10-07 12:27:02 -0400

    v1 of backend testing setup

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route('/api/test', methods=['GET'])
+def test_api():
+    response = {'message': 'Test successful nice!'}
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
